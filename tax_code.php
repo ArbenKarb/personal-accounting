e62848247676adff52ec4f61036941e74dedd024
ArbenKarb <31412198+ArbenKarb@users.noreply.github.com>
2020-03-26 22:19:41 +0100

    Update tax_code.php

--- tax_code.php
+++ tax_code.php
@@ -1,1 +1,16 @@
+<?php include "templates/header.php"; ?>
 
+<!DOCTYPE html>
+<html>
+
+<div class="tabContainer2">
+	<div class="buttonContainer2">
+		<button> Manage tax codes </button>
+	</div>
+
+</div>
+
+
+</html>
+
+<?php include "templates/footer.php"; ?>
